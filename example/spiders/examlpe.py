ab1cb9ccc32e36c2a231ebaa9c3cee6b50e37d5a
zhaomeng <1737605956@qq.com>
2020-05-05 16:26:52 +0800

    crawlspider案例

---
+++ example/spiders/examlpe.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+class ExamlpeSpider(CrawlSpider):
+    name = 'examlpe'
+    allowed_domains = ['daomubiji.com']
+    start_urls = ['http://daomubiji.com']
+
+    rules = (
+
+        Rule(LinkExtractor(allow=('qi-xing-lu-wang-',)), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        self.logger.info('Hi, this is an item page! %s', response.url)
+        item = scrapy.Item()
+        item['id'] = response.xpath('//h1[@class="article-title"]/text()').get()
+        item['name'] = response.xpath('//span[@class="item item-1"]/text()').get()
+        item['description'] = response.xpath('//article[@class="article-content"]//p/text()').get()
+        return item
